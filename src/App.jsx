79b0b3d414dfe8ec06404b026626e3b6fe9289ca
ejcabrera <ejcabrera021@gmail.com>
2023-10-24 00:50:42 +0800

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import NavBar from './components/NavBar'
-import SideBar from './components/Sidebar'
+import SideBar from './components/SideBar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <NavBar/>
+        <SideBar/>
+      </>
+    )
+  },
+  
+]);
 
 function App() {
+  
   return (
-    <>
-    <NavBar/>
-    <SideBar/>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,21 +1,54 @@
 import React, {useState} from 'react';
 import {AiFillHome, AiOutlineMenu} from 'react-icons/ai';
 import {ImCancelCircle} from 'react-icons/im';
-import { useNavigate, Link} from 'react-router-dom';
+import { useLocation, Link} from 'react-router-dom';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 
 
 const SideBar = ()=> {
   const [showSideBar, setShowSideBar] = useState(true);
+  const location = useLocation();
+  const currentPath = location.pathname
+
+  const activeLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#F51997] rounded';
+
+  const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold rounded';
 
   return (
     <div>
-      <div className="block xl:hidden m-2 ml-4 mt-3 text-xl" 
-      onClick={() => setShowSideBar((prev) => !prev)}>
-    {showSideBar ? <ImCancelCircle/>:<AiOutlineMenu />}
+      <div
+        className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+        onClick={() => setShowSideBar(!showSideBar)}
+      >
+        {showSideBar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
-
-      </div>
-  )
+      {showSideBar && (
+        <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3 '>
+          <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+            <Link to='/'>
+              <div className={currentPath === '/' ? activeLink : normalLink}>
+                <p className='text-2xl'>
+                  <AiFillHome />
+                </p>
+                <span className='capitalize text-xl hidden xl:block'>
+                  For You
+                </span>
+              </div>
+            </Link>
+          </div>
+          
+          <Discover />
+          <SuggestedAccounts
+            // fetchAllUsers={fetchAllUsers}
+            // allUsers={allUsers}
+          />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default SideBar
