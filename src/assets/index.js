d2de6c9cc79413284eac7cd31e827c1904251f96
aoropesa-amaoed <arjayoropesa23@gmail.com>
2023-11-18 17:16:18 +0800

    GoogleAuthentication/Feature need pa ng improvement do not merge until i say so. thank you

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+// import React, {useState} from 'react';
+// import axios from "axios";
+// import { jwtDecode } from 'jwt-decode';
+
+// const createOrGetUser = async (response) => {
+//     const decodedToken = jwtDecode(response.credential);
+//     console.log(decodedToken);
+// }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,79 @@
-import React from 'react'
-import Logo from '../assets/tiktik-logo.png'
-import { Link } from 'react-router-dom'
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
-
+import React from 'react';
+import Logo from '../assets/tiktik-logo.png';
+import { Link } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
+// import { createOrGetUser } from '../assets/index';
 
 const NavBar = () => {
-  const user = false;
-  return (   
+  const user = false; // You might want to replace this with your actual user state
+  return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
-      <Link to={'/'}>
+      <Link to='/'>
         <div className='w-[100px] md:w-[129px]'>
           <img className='cursor-pointer' src={Logo} alt="Tiktik" layout='responsive' />
         </div>
       </Link>
-      <div>SEARCH</div>  
+      <div>SEARCH</div>
       <div>
         {user ? (
           <div>Logged In</div>
         ) : (
-          <GoogleLogin 
-          onSuccess={(response)=>(
-            console.log(response)
-          )}
-          onError={()=> console.log('Error')}
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log('Error')}
           />
-        )
-
-        }
-      </div>    
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import Logo from '../assets/tiktik-logo.png'
+// import { Link } from 'react-router-dom'
+// import { GoogleLogin, googleLogout } from '@react-oauth/google';
+// import {createOrGetUser} from '../assets/index';
+
+
+// const NavBar = () => {
+//   const user = false;
+//   return (   
+//     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+//       <Link to='/'>
+//         <div className='w-[100px] md:w-[129px]'>
+//           <img className='cursor-pointer' src={Logo} alt="Tiktik" layout='responsive' />
+//         </div>
+//       </Link>
+//       <div>SEARCH</div>  
+//       <div>
+//         {user ? (
+//           <div>Logged In</div>
+//         ) : (
+//           <GoogleLogin 
+//           onSuccess={(response)=>(
+//             createOrGetUser(response)
+//           )}
+//           onError={()=> console.log('Error')}
+//           />
+//         )
+
+//         }
+//       </div>    
+//     </div>
+//   )
+// }
+
+// export default NavBar
